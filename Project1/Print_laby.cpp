7c4f619f0ac2f92d9d1652604faf66aa21a884ad
Htu_D <3170102733@zju.edu.cn>
2018-03-22 22:33:31 +0800

    lalala

--- Project1/Print_laby.cpp
+++ Project1/Print_laby.cpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "stdafx.h"
-#include"labyrinth.h"
 
 
 void Print_laby()
--- Project1/labyrinth.h
+++ Project1/labyrinth.h
@@ -2,23 +2,23 @@
 #ifndef _HEADER_H
 #define _HEADER_H
 
-#define MaxLength 20			//最大长度
-#define Random_Point_count 5					/*随机点的数量*/
+#define MaxLength 20				//最大长度
+#define Random_Point_count 5		//随机点的数量
 
 
-#define Blocked 0
-#define Available 1
-#define Passed 2
+#define Blocked 0					//墙
+#define Available 1					//通路
+#define Passed 2					//已经过的路线
 
-#define Start 1
-#define End size-2
+#define Start 1						//起始点
+#define End size-2					//终点
 
-extern size_t size;
-extern struct coordinate path[64];
-extern int laby[MaxLength][MaxLength];
+extern size_t size;					//迷宫的大小
+extern struct coordinate path[100];	//路径数组
+extern int laby[MaxLength][MaxLength];//迷宫数组
 
 
-struct coordinate	//坐标结构定义
+struct coordinate					//坐标结构定义
 {
 	size_t x;
 	size_t y;
