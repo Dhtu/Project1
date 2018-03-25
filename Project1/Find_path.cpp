030d9344c82b8e9e43b3736f4768370d69b26b1e
Htu_D <3170102733@zju.edu.cn>
2018-03-25 19:34:20 +0800

    增加了最短路径算法

--- Project1/Find_path.cpp
+++ Project1/Find_path.cpp
@@ -1,68 +1,68 @@
 #pragma once
 #include "stdafx.h"
+#define MAX 5
 
-static size_t sg_ulIndex;	        //path数组的下标
+static size_t sg_ulIndex, sg_ulMin;	        //path数组的下标
 static size_t sg_ulSize;
 static Point_S *sg_astPath;
 static int32_t **sg_pLaby;
-
+static Point_S *sg_astTest;
 
 
 int32_t Finding(size_t x, size_t y)
 {
 	size_t ulSign;
 
-	sg_astPath[sg_ulIndex].x = x;		//将当前位置插入栈顶
-	sg_astPath[sg_ulIndex].y = y;
+	sg_astTest[sg_ulIndex].x = x;		//将当前位置插入栈顶
+	sg_astTest[sg_ulIndex].y = y;
 	sg_ulIndex++;
 	ulSign = sg_ulIndex;			    //储存index，方便回退
 
 
 	if (x == END && y == END)           //判断是否到达终点
 	{
-		sg_astPath[sg_ulIndex].x = '#';  //终止符号
-		return AVAILABLE;
+		if (sg_ulIndex<sg_ulMin)//判断是否是最短路径
+		{
+			sg_ulMin = sg_ulIndex;
+			for (size_t i = 0; i < sg_ulIndex; i++)
+			{
+				sg_astPath[i] = sg_astTest[i];
+			}
+			sg_astPath[sg_ulIndex].x = -1;
+		}
 	}
 
-	                                    //通过不断尝试各个方向，直到遇到通路（Available）如果遇到死路（Blocked）则出栈
+	//通过不断尝试各个方向，直到遇到通路（Available）如果遇到死路（Blocked）则出栈
 	if (sg_pLaby[x + 1][y] == AVAILABLE)
 	{
 		sg_ulIndex = ulSign;
 		sg_pLaby[x + 1][y] = PASSED;
-		if (AVAILABLE == Finding(x + 1, y))
-		{
-			return AVAILABLE;
-		}
+		Finding(x + 1, y);
+		sg_pLaby[x + 1][y] = AVAILABLE;
 	}
 
 	if (sg_pLaby[x][y + 1] == AVAILABLE)
 	{
 		sg_ulIndex = ulSign;
 		sg_pLaby[x][y + 1] = PASSED;
-		if (AVAILABLE == Finding(x, y + 1))
-		{
-			return AVAILABLE;
-		}
+		Finding(x, y + 1);
+		sg_pLaby[x][y + 1] = AVAILABLE;
 	}
 
 	if (sg_pLaby[x - 1][y] == AVAILABLE)
 	{
 		sg_ulIndex = ulSign;
 		sg_pLaby[x - 1][y] = PASSED;
-		if (AVAILABLE == Finding(x - 1, y))
-		{
-			return AVAILABLE;
-		}
+		Finding(x - 1, y);
+		sg_pLaby[x - 1][y] = AVAILABLE;
 	}
 
 	if (sg_pLaby[x][y - 1] == AVAILABLE)
 	{
 		sg_ulIndex = ulSign;
 		sg_pLaby[x][y - 1] = PASSED;
-		if (AVAILABLE == Finding(x, y - 1))
-		{
-			return AVAILABLE;
-		}
+		Finding(x, y - 1);
+		sg_pLaby[x][y - 1] = AVAILABLE;
 	}
 
 
@@ -77,9 +77,9 @@ Point_S *Find_path(int32_t **pLaby, size_t ulSize)
 	sg_ulIndex = 0;
 	sg_ulSize = ulSize;
 	sg_pLaby = pLaby;
-	sg_astPath = (Point_S *)malloc(sizeof(Point_S) * ulSize * 5);
-
-	
+	sg_astPath = (Point_S *)malloc(sizeof(Point_S) * ulSize * ulSize);
+	sg_astTest = (Point_S *)malloc(sizeof(Point_S) * ulSize * ulSize);
+	sg_ulMin = ulSize * ulSize;
 	Finding(START, START);
 
 
--- Project1/Print_path.cpp
+++ Project1/Print_path.cpp
@@ -5,7 +5,7 @@ void Print_path(Point_S *p_astPath)
 {
 	size_t i;
 
-	for (i = 0; p_astPath[i].x!='#'; i++)
+	for (i = 0; p_astPath[i].x!=-1; i++)
 	{
 		printf_s("(%d,%d) -> ", p_astPath[i].x, p_astPath[i].y);
 	}
